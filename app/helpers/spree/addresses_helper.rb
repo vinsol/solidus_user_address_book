ff864c2938b68856c11befefd3f8bda25882351d
Akhil Bansal <bansalakhil30.10@gmail.com>
2017-04-10 15:01:20 +0530

    Updated views

--- app/helpers/spree/addresses_helper.rb
+++ app/helpers/spree/addresses_helper.rb
@@ -1,13 +1,13 @@
 module Spree::AddressesHelper
   def address_field(form, method, address_id = "b", &handler)
-    content_tag :p, :id => [address_id, method].join, :class => "form-group" do
+    content_tag :p, id: [address_id, method].join, class: "form-group" do
       if handler
         handler.call
       else
         is_required = Spree::Address.required_fields.include?(method)
         separator = is_required ? '<span class="required">*</span><br />' : '<br />'
         form.label(method) + separator.html_safe +
-        form.text_field(method, :class => [is_required ? 'required' : nil, 'form-control'].compact)
+        form.text_field(method, class: [is_required ? 'required' : nil, 'form-control'].compact)
       end
     end
   end
@@ -18,16 +18,16 @@ module Spree::AddressesHelper
     state_elements = [
       form.collection_select(:state_id, country.states.order(:name),
                             :id, :name,
-                            {:include_blank => true},
-                            {:class => have_states ? "required form-control" : "hidden",
-                            :disabled => !have_states}) +
+                            {include_blank: true},
+                            {class: have_states ? "required form-control" : "hidden",
+                            disabled: !have_states}) +
       form.text_field(:state_name,
-                      :class => !have_states ? "required" : "hidden",
-                      :disabled => have_states)
+                      class: !have_states ? "required" : "hidden",
+                      disabled: have_states)
       ].join.gsub('"', "'").gsub("\n", "")
 
     form.label(:state, t(:state)) + '<span class="req">*</span><br />'.html_safe +
-      content_tag(:noscript, form.text_field(:state_name, :class => 'required')) +
+      content_tag(:noscript, form.text_field(:state_name, class: 'required')) +
       javascript_tag("document.write(\"#{state_elements.html_safe}\");")
   end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,7 +3,7 @@
 ]).flatten!
 
 Spree::Order.class_eval do
-  before_validation :clone_shipping_address, :if => "SolidusUserAddressBook::Config[:disable_bill_address]"
+  before_validation :clone_shipping_address, if: "SolidusUserAddressBook::Config[:disable_bill_address]"
   
   def clone_shipping_address
     if self.ship_address
@@ -20,7 +20,7 @@ Spree::Order.class_eval do
   end
   
   def bill_address_id=(id)
-    address = Spree::Address.where(:id => id).first
+    address = Spree::Address.where(id: id).first
     if address && address.user_id == self.user_id
       self["bill_address_id"] = address.id
       self.user.update_attribute(:bill_address_id, address.id)
@@ -36,7 +36,7 @@ Spree::Order.class_eval do
   end
 
   def ship_address_id=(id)
-    address = Spree::Address.where(:id => id).first
+    address = Spree::Address.where(id: id).first
     if address && address.user_id == self.user_id
       self["ship_address_id"] = address.id
       self.user.update_attribute(:ship_address_id, address.id)
@@ -47,7 +47,7 @@ Spree::Order.class_eval do
   end
   
   def ship_address_attributes=(attributes)
-    self.ship_address = update_or_create_address(attributes)
+    self.ship_address      = update_or_create_address(attributes)
     self.user.ship_address = self.ship_address if self.user
   end
 
@@ -66,9 +66,9 @@ Spree::Order.class_eval do
 
   def update_addresses_params
     self.bill_address_attributes = @updating_params["order"].delete("bill_address_attributes")
-    self.bill_address_id = @updating_params["order"].delete("bill_address_id")
+    self.bill_address_id         = @updating_params["order"].delete("bill_address_id")
     self.ship_address_attributes = @updating_params["order"].delete("ship_address_attributes")
-    self.ship_address_id = @updating_params["order"].delete("ship_address_id")
+    self.ship_address_id         = @updating_params["order"].delete("ship_address_id")
   end
   
   def update_or_create_address(attributes = {})
